9d8389f253c3d496dbb9699291b23551a79e90ce
Filip Mellgren <filip.mellgren@gmail.com>
2022-03-30 15:05:08 +0200

    setup

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 #! /usr/bin/python
 
 # Main logic for the project, from here, everything building the project will be called upon.
+
+# Prepare data
+
+# Export data
+
+# Create output from exported data
+
+# Compile paper/presentation using new output

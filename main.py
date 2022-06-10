65dce2f60887a3f77abf02300a6050095b32019d
Filip Mellgren <filip.mellgren@gmail.com>
2022-06-10 14:49:39 +0200

    compile paper from run

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 # Main logic for the project, from here, everything building the project will be called upon.
 import os
 
-
 # Load data and store more conveniently
 from src.data_creation.c0_parse_data import parse_data
 if not os.path.exists(os.getcwd() + '/data/analysis/test2.csv'):
@@ -12,5 +11,7 @@ if not os.path.exists(os.getcwd() + '/data/analysis/test2.csv'):
 # Create output from exported data
 
 # Compile paper/presentation using new output
+os.system("pdflatex docs/paper.tex")
+os.system("mv paper.* docs")
 
 print("DONE!")

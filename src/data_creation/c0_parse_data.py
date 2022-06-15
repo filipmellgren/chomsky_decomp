7279a45b18e8bc0dc80538638faae33a44bd2d59
Filip Mellgren <filip.mellgren@gmail.com>
2022-06-15 14:50:58 +0200

    repackage code a bit

--- src/data_creation/c0_parse_data.py
+++ src/data_creation/c0_parse_data.py
@@ -30,7 +30,7 @@ def parse_data():
 	# From extracted .tgz files, locate countires that we care about inside articles.
 	# For articles with country we care about, save key attributes about the article and store in a list.
 	# Finally, write the list to a .csv file that will be used to analyze the data.
-	PATH = os.getcwd() + '/data/'
+	data_path = os.getcwd() + '/data/'
 	RAW = 'raw/nyt_corpus/data/'
 	DEST = 'extracted/'
 	
@@ -38,56 +38,58 @@ def parse_data():
 	years = read_params("parameters/years_lite.csv")
 	months = read_params("parameters/months_lite.csv")
 	
-	year_months = itertools.product(years, months)
+	prepare_tarfiles(years, months, data_path, DEST, RAW)
 
-	# Extract content from compressed TGZ file. Each TGZ file represents a month with the news article from each day in that month 
+	pathindex_file = "data/index/" +str(min(years)) + "_" + str(max(years)) +".csv" # TODO: not perfect indicator, lacks month. Not vital as main run will include everything anyway
 
-	for ym in year_months:
-		# Loop over cartesian product of years and months to extract TGZ files one by one.
-		if not os.path.exists(PATH + DEST + ym[0] + "/" + ym[1]):
-			# Only extract if destination file does not yet exist
-			extract_tarfile(PATH, DEST, RAW, ym[0], ym[1])
-
-	# Obtain a list of datapaths to extracted files.
-	datapaths = []
-
-	pathindex_file = "data/index/" +str(min(years)) + "_" + str(max(years)) +".csv" # TODO: not perfect indicator, lacks month
-
-	# TODO: check from an index containing path and a countrylist
-	if os.path.exists(pathindex_file): # If a path to files has already been created
-		datapaths_pd = pd.read_csv(pathindex_file)
-		datapaths_pd.columns = ['path', 'locations']
-		# TODO filter away those with irrelevant locations
-		# TODO: query might work
-		# if any("abc" in s for s in countries):
-		datapaths_pd = datapaths_pd.dropna() # Quick way to do some filtration on paths (drop all with no location)
-		datapaths_pd = datapaths_pd.assign(path=lambda x: os.getcwd() + x.path)
-		datapaths = list(datapaths_pd["path"])
-	else: # Otherwise go through all files
-		for root, dirs, files in os.walk(PATH + 'extracted/'):
-			for file in files:
-				if file.endswith(".xml"):
-					datapaths.append(root + "/" + file)
-					#print(os.path.join(root, file))
-
-	obs_dict_list = []
+	datapaths = get_paths(pathindex_file, data_path)
 	
 	# Parallel processing over datapaths
 	pool = Pool(cpu_count()-1)
 	obs_dict_list = pool.starmap(article_to_datarow, zip(datapaths, repeat(countries)))
 
 	datarows = [d['datarow'] for d in obs_dict_list]
-	# Filter away None entries
-	datarows = list(filter(None, datarows))
+	datarows = list(filter(None, datarows)) # filters away None entries
 
-	if not os.path.exists(pathindex_file):
-		path_indices = [d['pathindex'] for d in obs_dict_list]
-		write_list_to_table(path_indices, pathindex_file)
+	create_path_index(pathindex_file, obs_dict_list) 
 
 	write_to_file = "data/analysis/test.csv"
 	write_dict_to_table(datarows, write_to_file)
 
 	return
+
+def create_path_index(pathindex_file, obs_dict_list):
+	# Creates a path index for fast referencing of files by get_paths().
+	# Only create if it does not already exist.
+	if not os.path.exists(pathindex_file):
+		path_indices = [d['pathindex'] for d in obs_dict_list]
+		write_list_to_table(path_indices, pathindex_file)
+		return
+
+def get_paths(pathindex_file, data_path):
+		# Returns a list of paths to news articles.
+		# First, it checks if there exists an index over paths, and makes a selection based on the index.
+		# TODO: So far, the selection is dumb and only filters away articles that do not mention a location. Can make smarter using pd.query.
+		# If an index does not exist, the function adds all paths to all files that has been extracted.
+		# TODO: We can change behavior so it only returns relevant paths, depending on year and month.
+
+	if os.path.exists(pathindex_file): # If a path to files has already been created
+		datapaths_pd = pd.read_csv(pathindex_file)
+		datapaths_pd.columns = ['path', 'locations']
+		# TODO filter away those with irrelevant locations 
+		# TODO: query might work
+		# if any("abc" in s for s in countries):
+		datapaths_pd = datapaths_pd.dropna() # Quick way to do some filtration on paths (drop all with no location)
+		datapaths_pd = datapaths_pd.assign(path=lambda x: os.getcwd() + x.path)
+		datapaths = list(datapaths_pd["path"])
+	else: # Otherwise go through all files. TODO: this goes through all extracted files. Not always what we want and time consuming. 
+		datapaths = []
+		for root, dirs, files in os.walk(data_path + 'extracted/'):
+			for file in files:
+				if file.endswith(".xml"):
+					datapaths.append(root + "/" + file)
+					#print(os.path.join(root, file))
+	return datapaths
 
 def read_params(param_path):
 	# Utility function to read parameter files
@@ -95,6 +97,18 @@ def read_params(param_path):
 	params = f.read().split(', ')
 	f.close()
 	return(params)
+
+def prepare_tarfiles(years, months, data_path, DEST, RAW):
+# Checks whether tarfiles have been extracted, if not, it calls on extract_tarfile()
+
+	year_months = itertools.product(years, months)
+	# Extract content from compressed TGZ file. Each TGZ file represents a month with the news article from each day in that month 
+	for ym in year_months:
+		# Loop over cartesian product of years and months to extract TGZ files one by one.
+		if not os.path.exists(data_path + DEST + ym[0] + "/" + ym[1]):
+			# Only extract if destination file does not yet exist
+			extract_tarfile(data_path, DEST, RAW, ym[0], ym[1])	
+	return
 
 def extract_tarfile(path, dest, raw, year, month):
 	# Extract file if it exists
